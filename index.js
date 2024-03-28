eb2f27a4b98b131bada74a81a6ec3337f30a5b72
Nathaniel <nathaniel.jaden@student.moringaschool.com>
2024-03-28 14:31:29 +0300

    The JavaScript DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+});
+
+document.getElementById("text").innerHTML = "This is really cool!";
+// text.textContent = "JavaScript is so cool. It lets me add text to my page programmatically.";
